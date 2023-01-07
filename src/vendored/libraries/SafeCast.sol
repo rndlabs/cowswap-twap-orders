136a083c6df2c4165d1b300aadc90e0378be9780
mfw78 <mfw78@protonmail.com>
2023-01-07 07:17:04 +0000

    feat: toUint32

--- src/vendored/libraries/SafeCast.sol
+++ src/vendored/libraries/SafeCast.sol
@@ -59,4 +59,21 @@ library SafeCast {
         );
         return int256(value);
     }
+
+    /**
+     * @dev Returns the downcasted uint32 from uint256, reverting on
+     * overflow (when the input is greater than largest uint32).
+     *
+     * Counterpart to Solidity's `uint32` operator.
+     *
+     * Requirements:
+     *
+     * - input must fit into 32 bits
+     *
+     * _Available since v2.5._
+     */
+    function toUint32(uint256 value) internal pure returns (uint32) {
+        require(value <= type(uint32).max, "SafeCast: value doesn't fit in 32 bits");
+        return uint32(value);
+    }
 }
