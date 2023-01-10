6c861e3de100c0842762f159cca80299d53b428c
mfw78 <mfw78@protonmail.com>
2023-01-10 07:19:32 +0000

    refactor: Safe base helper

--- test/helpers/Safe.sol
+++ test/helpers/Safe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
+import {Enum} from "../../lib/safe/contracts/common/Enum.sol";
 import {GnosisSafe} from "../../lib/safe/contracts/GnosisSafe.sol";
 import {GnosisSafeProxy} from "../../lib/safe/contracts/proxies/GnosisSafeProxy.sol";
 import {GnosisSafeProxyFactory} from "../../lib/safe/contracts/proxies/GnosisSafeProxyFactory.sol";
@@ -56,6 +57,58 @@ abstract contract Safe {
                 ),
                 0 // nonce
             )
+        );
+    }
+
+    // @dev Executes a transaction on a Gnosis Safe proxy with the provided accounts as signatories.
+    // @param safe The Gnosis Safe proxy.
+    // @param to The address to which the transaction is sent. If set to 0x0, the transaction is sent to the Safe.
+    // @param value The amount of Ether to be sent with the transaction.
+    // @param data The data to be sent with the transaction.
+    // @param operation The operation to be performed.
+    // @param signers The accounts that will sign the transaction.    
+    function execute(
+        GnosisSafe safe,
+        address to,
+        uint256 value,
+        bytes memory data,
+        Enum.Operation operation,
+        TestAccount[] memory signers
+    ) internal {
+        uint256 nonce = safe.nonce();
+        bytes32 setHandlerTx = safe.getTransactionHash(
+            to, 
+            value,
+            data,
+            operation,
+            0,
+            0,
+            0,
+            address(0),
+            address(0),
+            nonce
+        );
+
+        // sign the transaction by alice and bob (sort their account by ascending order)
+        signers = signers.sortAccounts();
+
+        bytes memory signatures;
+        for (uint256 i = 0; i < signers.length; i++) {
+            signatures = abi.encodePacked(signatures, signers[i].signPacked(setHandlerTx));
+        }
+
+        // execute the transaction
+        safe.execTransaction(
+            to, 
+            value,
+            data,
+            operation,
+            0,
+            0,
+            0,
+            address(0),
+            payable(0),
+            abi.encodePacked(signatures)
         );
     }
 }
