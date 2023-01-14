9392cd996cf0027db4df48f551e55bcea807efae
mfw78 <mfw78@protonmail.com>
2023-01-14 12:29:33 +0000

    tests: expand base helper functionality

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -50,9 +50,100 @@ abstract contract Base is Test, Tokens, Safe, CoWProtocol {
     }
 
     function signers() internal view returns (TestAccount[] memory) {
-        TestAccount[] memory signers = new TestAccount[](2);
-        signers[0] = alice;
-        signers[1] = bob;
-        return signers;
+
+    function setFallbackHandler(GnosisSafe safe, CoWFallbackHandler handler) internal {
+        // do the transaction
+        execute(
+            safe,
+            address(safe),
+            0,
+            abi.encodeWithSelector(
+                safe.setFallbackHandler.selector,
+                address(handler)
+            ),
+            Enum.Operation.Call,
+            signers()
+        );
+    }
+
+    function safeSignMessage(
+        GnosisSafe safe,
+        bytes memory message
+    ) internal {
+        execute(
+            safe,
+            address(signMessageLib),
+            0,
+            abi.encodeWithSelector(
+                signMessageLib.signMessage.selector,
+                message
+            ),
+            Enum.Operation.DelegateCall,
+            signers()
+        );
+    }
+
+    function createOrder(
+        GnosisSafe safe,
+        bytes memory conditionalOrder,
+        IERC20 sellToken,
+        uint256 sellAmount
+    ) internal {
+        // Hash of the conditional order to sign
+        bytes32 typedHash = ConditionalOrderLib.hash(conditionalOrder, settlement.domainSeparator());
+
+        bytes memory signMessageTx = abi.encodeWithSelector(
+            signMessageLib.signMessage.selector,
+            abi.encode(typedHash)
+        );
+
+        bytes memory approveTx = abi.encodeWithSelector(
+            sellToken.approve.selector,
+            address(relayer),
+            sellAmount
+        );
+
+        bytes memory dispatchTx = abi.encodeWithSelector(
+            CoWFallbackHandler(address(safe)).dispatch.selector,
+            conditionalOrder
+        );
+
+        /// @dev Using the `multisend` contract to batch multiple transactions
+        execute(
+            safe,
+            address(multisend),
+            0,
+            abi.encodeWithSelector(
+                multisend.multiSend.selector, 
+                abi.encodePacked(
+                    // 1. sign the conditional order
+                    abi.encodePacked(
+                        uint8(Enum.Operation.DelegateCall),
+                        address(signMessageLib),
+                        uint256(0),
+                        signMessageTx.length,
+                        signMessageTx
+                    ),
+                    // 2. approve the tokens to be spent by the settlement contract
+                    abi.encodePacked(
+                        Enum.Operation.Call,
+                        address(sellToken),
+                        uint256(0),
+                        approveTx.length,
+                        approveTx
+                    ),
+                    // 3. dispatch the conditional order
+                    abi.encodePacked(
+                        Enum.Operation.Call,
+                        address(safe),
+                        uint256(0),
+                        dispatchTx.length,
+                        dispatchTx
+                    )
+                )
+            ),
+            Enum.Operation.DelegateCall,
+            signers()
+        );
     }
 }
