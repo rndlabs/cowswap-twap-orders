ca6ded0cedcefe70ec4c7877fcc38031c5894a9b
mfw78 <mfw78@protonmail.com>
2023-01-20 10:00:03 +0000

    feat: uint256 to avoid overflow

--- src/libraries/TWAPOrderMathLib.sol
+++ src/libraries/TWAPOrderMathLib.sol
@@ -17,11 +17,11 @@ library TWAPOrderMathLib {
     /// @param frequency The frequency of each part.
     /// @param span The span of each part.
     function calculateValidTo(
-        uint32 currentTime,
-        uint32 startTime,
-        uint32 numParts,
-        uint32 frequency,
-        uint32 span
+        uint256 currentTime,
+        uint256 startTime,
+        uint256 numParts,
+        uint256 frequency,
+        uint256 span
     ) internal pure returns (uint256 validTo) {
         /// @dev Use `assert` to check for invalid inputs as these should be caught by the
         /// conditional order validation logic in `dispatch` before calling this function.
@@ -30,25 +30,25 @@ library TWAPOrderMathLib {
         assert(frequency > 0 && frequency <= 365 days);
         assert(span <= frequency);
 
-        // Order is not valid before the start (order commences at `t0`).
+        /// @dev Order is not valid before the start (order commences at `t0`).
         if (!(startTime <= currentTime)) revert ConditionalOrder.OrderNotValid();
-        // Order is expired after the last part (`n` parts, running at `t` time length).
+        /// @dev Order is expired after the last part (`n` parts, running at `t` time length).
         if (!(currentTime < startTime + (numParts * frequency))) revert ConditionalOrder.OrderExpired();
 
         // Get the TWAP order part number (indexed from 0)
-        uint256 part = (currentTime - startTime) / frequency;
+        uint256 part;
+        unchecked {
+            part = (currentTime - startTime) / frequency;
+        }
         // The part number MUST be less than the total number of parts (`n`)
         assert(part < numParts);
         // calculate the `validTo` timestamp (inclusive as per `GPv2Order`)
-        validTo = (span == 0 ? startTime + ((part + 1) * frequency) : startTime + (part * frequency) + span) - 1;
+        unchecked {
+            validTo = (span == 0 ? startTime + ((part + 1) * frequency) : startTime + (part * frequency) + span) - 1;
+        }
 
-        // Order is not valid if not within nominated span
+        /// @dev Order is not valid if not within nominated span
         if (!(currentTime <= validTo)) revert ConditionalOrder.OrderNotValid();
-
-        // `validTo` MUST be now or in the future.
-        assert(validTo >= currentTime);
-        // `validTo` MUST be before the next part.
-        assert(validTo == startTime + ((part + 1) * frequency) - (span != 0 ? (frequency - span) : 0) - 1);
     }
 
     /// @dev Calculate the part limit for a TWAP order.
@@ -72,7 +72,6 @@ library TWAPOrderMathLib {
         /// @dev We determine the part limit for a TWAP order, which is the maximum
         /// amount of buyToken that can be bought for a unit of sellToken.
 
-        // get the part to sell
         /// @dev Example we are selling 100,000 DAI in 10 parts for WETH (lots of 10,000 DAI).
         ///      A limit price of of 1500 DAI/WETH means we do not want to pay more than 1500 DAI for 1 WETH.
         ///      Therefore in each part, we require a minimum of 10,000 / 1500 = 6.666666666666666666 WETH.
